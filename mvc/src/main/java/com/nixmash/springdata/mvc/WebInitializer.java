cd01e976727922bac2cd2c705e2b65a9c1dcf93a
mintster <daveburkevt@gmail.com>
2016-03-25 14:06:05 -0400

    static/css access fix

--- mvc/src/main/java/com/nixmash/springdata/mvc/WebInitializer.java
+++ mvc/src/main/java/com/nixmash/springdata/mvc/WebInitializer.java
@@ -1,12 +1,10 @@
 package com.nixmash.springdata.mvc;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import com.nixmash.springdata.mvc.config.Application;
 
-@SpringBootApplication
 public class WebInitializer extends SpringBootServletInitializer {
 
     @Override
