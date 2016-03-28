b267dcb984e06983f53fde21d48dd440afa3368b
mintster <daveburkevt@gmail.com>
2016-03-28 13:29:06 -0400

    ignored resources mod

--- mvc/src/main/java/com/nixmash/springdata/mvc/config/SecurityConfig.java
+++ mvc/src/main/java/com/nixmash/springdata/mvc/config/SecurityConfig.java
@@ -30,7 +30,7 @@ import com.nixmash.springdata.mvc.security.SimpleSocialUserDetailsService;
 @ComponentScan(basePackageClasses = CurrentUserDetailsService.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private static final String[] IGNORED_RESOURCE_LIST = new String[] {"/css/**", "/img/**", "/fonts/**", "/webjars/**" };
+	private static final String[] IGNORED_RESOURCE_LIST = new String[] {"/fonts/**", "/webjars/**" };
 	private static final String[] PERMITALL_RESOURCE_LIST = new String[] {"/auth/**", "/signin/**", "/signup/**", "/",
 			"/register/**", "/contacts", "/json/**", "/products/**" };
 	private static final String[] ADMIN_RESOURCE_LIST = new String[] { "/admin/**" };
